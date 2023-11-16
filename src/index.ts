ec0f475cbf30b9c6bd6026da08437061b8a64705
LouisFaker <tr4faker@gmail.com>
2023-11-16 08:30:46 -0400

    agregando opção de usar o post para salvar filme

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
 import express, {Request} from 'express';
-
-// Cria uma instância do aplicativo Express
+import BancoMongoDB from './infra/banco/banco-mongodb' //paso 2: banco de dados add
+import ListarFilme from './aplicacao/listar-filme.use-case' //paso 1: adiconar import listarfilme
+const bancoMongoDB = new BancoMongoDB();
 const app = express();
 app.use(express.json())
 
-type Filme = {
-    id: number,
-    titulo: string,
-    descricao: string,
-    foto: string,
-}
-let filmes_repositorio:Filme[] = [] //usar no lugar usecase para listar filmes e usar para que este listar aquil no banco de dados
 
+app.get('/filmes', async (req, res) => {
+    const listarFilme = new ListarFilme(bancoMongoDB)
+    const filmes = await listarFilme.execute()
+    // res.send(filmes).status(200)
+    res.status(200).send(filmes) //passar primeiro status seguido do send
+})
 
-// Define uma rota padrão
-app.get('/filmes/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    const filme = filmes_repositorio.find(filme => filme.id === id)
-    if (!filme) res.status(404).send()
-    res.send(filme)        
-});
+// // Define uma rota padrão
+// app.get('/filmes/:id', (req, res) => {
+//     const id = parseInt(req.params.id)
+//     const filme = filmes_repositorio.find(filme => filme.id === id)
+//     if (!filme) res.status(404).send()
+//     res.send(filme)        
+// });
 
-app.post('/filmes', (req:Request, res) => {
+app.post('/filmes', async (req:Request, res) => {
     const {id, titulo, descricao, foto} = req.body
     const filme:Filme = {
         id,
@@ -30,6 +30,7 @@ app.post('/filmes', (req:Request, res) => {
         foto,
     }
     filmes_repositorio.push(filme)
+    await bancoMongoDB.salvar(filme)
     res.status(201).send(filme)
 });
 
@@ -42,8 +43,14 @@ app.delete('/filmes/:id', (req, res) => {
     res.status(200).send(filme)
 });
 
-
-// Inicia o servidor
 app.listen(3000, () => {
     console.log('Servidor iniciado na porta 3000');
 });
+
+type Filme = {
+    id: number,
+    titulo: string,
+    descricao: string,
+    foto: string,
+}
+let filmes_repositorio:Filme[] = [] //usar no lugar usecase para listar filmes e usar para que este listar aquil no banco de dados
